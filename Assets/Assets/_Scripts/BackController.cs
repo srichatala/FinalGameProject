0520e9c375833f421378957a0f373006d320ad6b
srichatala <sri2vas@gmail.com>
2015-11-29 14:39:23 -0500

    Initial files added and Instruction, Playgame, Exit scenes added to game

---
+++ Assets/Assets/_Scripts/BackController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnStartButton()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
---
+++ Assets/Assets/_Scripts/PlayGameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnStartButton()
+    {
+        Application.LoadLevel("Level1");
+    }
+}
