962a882de7f976ce7192979e057228e164bc8d4a
srichatala <sri2vas@gmail.com>
2015-11-29 21:07:35 -0500

    level background added to scene

---
+++ Assets/_Scripts/RoadController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadController : MonoBehaviour {
+
+
+    //Public variables
+    public float Speed;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 currentPosition = new Vector2(0.0f, 0.0f);
+        currentPosition = gameObject.GetComponent<Transform>().position;
+        currentPosition.y -= Speed;
+
+        gameObject.GetComponent<Transform>().position = currentPosition;
+        if (currentPosition.y <= -3.5)
+        {
+            this._Reset();
+        }
+    }
+    private void _Reset()
+    {
+        Vector2 resetPosition = new Vector2(0.0f, 3.5f);
+        gameObject.GetComponent<Transform>().position = resetPosition;
+    }
+}
